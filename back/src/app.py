b4da97edfd7d39c1cc85b45cbce13c920894511f
Sergey Natalenko <sergey.natalenko@mail.ru>
2023-03-26 17:46:38 +0300

    Fix back

--- back/src/app.py
+++ back/src/app.py
@@ -45,34 +45,4 @@ def create_app():
         generate_schemas=True,
         add_exception_handlers=True,
     )
-
-    @app.get("/me")
-    async def get_me(user: User = Depends(get_current_user)):
-        return await User_Pydantic.from_tortoise_orm(user)
-
-
-    @app.get("/preferences")
-    async def get_preferences(preferences: Preferences = Depends(get_user_preferences)):
-        return await Preferences_Pydantic.from_tortoise_orm(preferences)
-
-
-    app.include_router(auth_router)
-    app.include_router(video_router)
-
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=["*"],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-        expose_headers=["*"],
-    )
-
-    register_tortoise(
-        app,
-        db_url=f"postgres://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:5432/{POSTGRES_DB}",
-        modules={"models": ["db.models"]},
-        generate_schemas=True,
-        add_exception_handlers=True,
-    )
     return app
