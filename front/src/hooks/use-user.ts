a11a9327aa16e68075f1ae585945e4e4bf302100
nevsky118 <nevsky118@gmail.com>
2023-03-28 12:18:51 +0300

    Replace with query

--- front/src/hooks/use-user.ts
+++ front/src/hooks/use-user.ts
@@ -1,9 +1,20 @@
-import { RootState } from '@/store';
-import { useMemo } from 'react';
+import { useGetUserQuery } from '@/redux/services/user';
+import { RootState } from '@/redux/store';
+import { useEffect, useMemo } from 'react';
 import { useSelector } from 'react-redux';
 
 export const useUser = () => {
-	const user = useSelector((state: RootState) => state.auth.user);
+	// const user = useSelector((state: RootState) => state.auth.user);
 
-	return useMemo(() => ({ user }), [user]);
+	const { data: user, isLoading } = useGetUserQuery();
+
+	console.log('useUser user', user, isLoading);
+
+	useEffect(() => {
+		if (!isLoading && !user) {
+			// Handle error or redirect to login page
+		}
+	}, [isLoading, user]);
+
+	return { user, isLoading };
 };
